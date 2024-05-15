989b229e8b44ba4fdb019b115bd3787f035bda2d
Omar Ayman <102927278+omaraymanatia@users.noreply.github.com>
2024-05-15 19:00:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,14 +7,5 @@
 #include"BST.h"
 using namespace std;
 int main() {
-	BST<int>tree;
-	tree.Insert(20);
-	tree.Insert(40);
-	tree.Insert(10);
-	tree.Insert(41);
-	tree.Insert(30);
-	tree.Insert(15);
-	tree.Insert(9);
-	tree.Remove(9);
-	tree.PostOrderTraversal();
+	
 }
